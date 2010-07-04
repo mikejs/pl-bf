a13b5428abbaa99c44e916792b2b5c2519d817b9
Michael Stephens <me@mikej.st>
2010-07-04 03:39:15 -0400

    some input

--- pl_bf.c
+++ pl_bf.c
@@ -17,44 +17,126 @@ PG_FUNCTION_INFO_V1(pl_bf_call_handler);
 
 int interpret(char*, int, char*);
 
+size_t append_datum(char* buf, Datum val, bool isnull, Oid typeoid)
+{
+    HeapTuple typeTup;
+    Form_pg_type typeStruct;
+    FmgrInfo tmp_flinfo;
+    char *str;
+    size_t len;
+
+    typeTup = SearchSysCache(TYPEOID, ObjectIdGetDatum(typeoid), 0, 0, 0);
+    if (!HeapTupleIsValid(typeTup)) {
+        elog(ERROR, "Cache lookup failed for %u", typeoid);
+    }
+    typeStruct = (Form_pg_type)GETSTRUCT(typeTup);
+
+    if (typeStruct->typtype != 'b') {
+        // Non-basic type
+        elog(ERROR, "Don't support non-basic types (%s)",
+             format_type_be(typeoid));
+    }
+
+    fmgr_info_cxt(typeStruct->typoutput, &tmp_flinfo, CurTransactionContext);
+
+    ReleaseSysCache(typeTup);
+    
+    if (!isnull) {
+        if (typeoid == INT4OID) {
+            *((int*)buf) = DatumGetInt32(val);
+            return 4;
+        }
+        
+        SPI_push();
+        str = OutputFunctionCall(&tmp_flinfo, val);
+        SPI_pop();
+        len = strlen(str);
+        strncpy(buf, str, len);
+        return len;
+    }
+
+    return 0;
+}
+
 Datum pl_bf_call_handler(PG_FUNCTION_ARGS)
 {
+    FmgrInfo flinfo;
     Datum retval, proc_source_datum;
     Form_pg_proc procStruct;
+    Form_pg_type typeStruct;
     HeapTuple procTup;
+    HeapTuple typeTup;
     bool isnull;
+    Oid resultTypeIOParam, returnTypeOID;
     char *proc_source;
-    char *a;
+    char *a, *p;
+    int i;
 
+    // Get the function tuple
     procTup = SearchSysCache(PROCOID, ObjectIdGetDatum(fcinfo->flinfo->fn_oid),
                              0, 0, 0);
-
     if (!HeapTupleIsValid(procTup)) {
         elog(ERROR, "Cache lookup failed for procedure %u",
              fcinfo->flinfo->fn_oid);
     }
-
     procStruct = (Form_pg_proc)GETSTRUCT(procTup);
-
-    ReleaseSysCache(procTup);
-
+    
+    // Get the function source
     proc_source_datum = SysCacheGetAttr(PROCOID, procTup,
                                         Anum_pg_proc_prosrc, &isnull);
-
     if (isnull) {
         elog(ERROR, "Function source is null");
     }
-
     proc_source = DatumGetCString(DirectFunctionCall1(textout,
                                                       proc_source_datum));
 
+    // Get the return type tuple
+    typeTup = SearchSysCache(TYPEOID, ObjectIdGetDatum(procStruct->prorettype),
+                             0, 0, 0);
+    if (!HeapTupleIsValid(typeTup)) {
+        elog(ERROR, "Cache lookup failed for type %u", procStruct->prorettype);
+    }
+    typeStruct = (Form_pg_type)GETSTRUCT(typeTup);
+    
+    resultTypeIOParam = getTypeIOParam(typeTup);
+    returnTypeOID = procStruct -> prorettype;
+
     a = calloc(5000, 1);
+    if (!a) {
+        elog(ERROR, "BAD A!");
+    }
+
+    p = a;
+    for (i = 0; i < procStruct->pronargs; i++) {
+        p += append_datum(p, fcinfo->arg[i], fcinfo->argnull[i],
+                          procStruct->proargtypes.values[i]);
+    }
     
     interpret(a, 0, proc_source);
 
-    retval = Int32GetDatum(*((int*)a));
+    fmgr_info_cxt(typeStruct->typinput, &flinfo, TopMemoryContext);
 
+    if (returnTypeOID != VOIDOID) {
+        if (returnTypeOID == INT4OID) {
+            retval = Int32GetDatum(*((int*)a));
+        } else {
+            SPI_push();
+            if (returnTypeOID == BOOLOID)
+                retval = InputFunctionCall(&flinfo, a[0] ? "TRUE" : "FALSE",
+                                           resultTypeIOParam, -1);
+            else {
+                retval = InputFunctionCall(&flinfo, pstrdup(a),
+                                           resultTypeIOParam, -1);
+            }
+            SPI_pop();
+        }
+    }
+
+    ReleaseSysCache(procTup);
+    ReleaseSysCache(typeTup);
     free(a);
+
+    SPI_finish();
     
     return retval;
 }
